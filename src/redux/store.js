5974e2d2f6671217d32b8e328d5426d9264ab586
tonilingardsson <toni.lingardsson@gmail.com>
2026-09-18 10:18:52 +0200

    Add comments to store.js for better understanding of Redux store functionality

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cardsReducer from './cardSlice';
 
+// For me to learn: This is where important app data lives. 
+// Any component connected (import {store}...) to it can read the data
 export const store = configureStore({
   reducer: {
     wallet: cardsReducer,
