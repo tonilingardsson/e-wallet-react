19d6b3bf358e232b06b48f876d6714e9cd538480
tonilingardsson <toni.lingardsson@gmail.com>
2026-09-18 11:37:09 +0200

    Refactor application structure by renamig old components to jsx instead of js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Wallet from './components/Wallet';
-import AddCard from './views/AddCard.jsx';
+import Wallet from './components/Wallet.js';
+import AddCard from './views/AddCard.js';
 
 function App() {
   return (
